807476af293fd2f6ca4d958fca74ca51b66b7fa0
FedorByssov <bysov1987@mail.ru>
2023-03-18 14:16:32 +0300

    Create first layouts

--- app/src/main/java/com/example/googleone/MainActivity.kt
+++ app/src/main/java/com/example/googleone/MainActivity.kt
@@ -3,9 +3,16 @@ package com.example.googleone
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,18 +20,40 @@ class MainActivity : ComponentActivity() {
         setContent {
             //  text function
             //  Text(text = "Hello World!")
-            MessageCard(name ="android")
+            MessageCard(Message("Fedor", "AndroidStart"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello World $name!")
+fun MessageCard(msg: Message) {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.humen),
+            contentDescription = "Humen",
+            // Set image size to 40 dp
+            modifier = Modifier
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.size(8.dp))
+        
+        Column() {
+            Text(msg.author)
+            Spacer(modifier = Modifier.size(4.dp))
+            Text(msg.body)
+        }
+    }
+
 }
 
 @Preview
 @Composable
-fun PreviewMessageCard(){
-    MessageCard(name ="Android")
+fun PreviewMessageCard() {
+    MessageCard(Message("Fedor", "AndroidStart"))
 }
