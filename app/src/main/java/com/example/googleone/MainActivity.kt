5ea2b2791f74ef3605375462a761aea58f169a88
FedorByssov <bysov1987@mail.ru>
2023-03-18 13:38:35 +0300

    Create text function

--- app/src/main/java/com/example/googleone/MainActivity.kt
+++ app/src/main/java/com/example/googleone/MainActivity.kt
@@ -3,41 +3,28 @@ package com.example.googleone
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.googleone.ui.theme.GoogleOneTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GoogleOneTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            //  text function
+            //  Text(text = "Hello World!")
+            MessageCard(name ="android")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(name: String) {
+    Text(text = "Hello World $name!")
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    GoogleOneTheme {
-        Greeting("Android")
-    }
+fun PreviewMessageCard(){
+    MessageCard(name ="Android")
 }
